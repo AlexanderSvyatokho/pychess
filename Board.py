948a7729665cc6690204a6a6c565a05a21bdb2bd
Alexander Svyatokho <alexander.svyatokho@gmail.com>
2024-11-19 18:02:40 +0200

    Other pieces movement logic

--- Board.py
+++ Board.py
@@ -9,6 +9,7 @@ class Board:
 
     def setToDefault(self):
         self.turn = 'W'
+        # Set up the board with pieces
         pieces = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
         for i in range(8):
             self.board[i][0] = 'W' + pieces[i]
@@ -36,15 +37,20 @@ class Board:
             return self.getValidMovesForPawn(x, y)
         elif (piece[1] == 'R'):
             return self.getValidMovesForRook(x, y)
-        
-        return []
-    
-    def getValidMovesForPawn(self, x, y):
-        piece = self.getPiece(x, y)
-        if (not piece or piece[1] != 'P'):
-            logging.warning(f'Board: getValidMovesForPawn: no pawn at {x, y}')
+        elif (piece[1] == 'N'):
+            return self.getValidMovesForKnight(x, y)
+        elif (piece[1] == 'B'):
+            return self.getValidMovesForBishop(x, y)
+        elif (piece[1] == 'Q'):
+            return self.getValidMovesForQueen(x, y)
+        elif (piece[1] == 'K'):
+            return self.getValidMovesForKing(x, y)
+        else:
+            logging.error('Board: getValidMovesForPiece: unknown piece type')
             return []
     
+    # Precondition: pawn at x, y
+    def getValidMovesForPawn(self, x, y):
         moves = []
         direction = 1 if self.turn == 'W' else -1
 
@@ -62,12 +68,8 @@ class Board:
                 moves.append((x + 1, y + direction))
         return moves
     
+    # Precondition: rook at x, y
     def getValidMovesForRook(self, x, y):
-        piece = self.getPiece(x, y)
-        if (not piece or piece[1] != 'R'):
-            logging.warning(f'Board: getValidMovesForRook: no rook at {x, y}')
-            return []
-    
         moves = []
 
         # Horizontal moves to the right
@@ -113,6 +115,170 @@ class Board:
                     break
                 else:
                     break   
+
+        return moves
+    
+    # Precondition: knigh at x, y
+    def getValidMovesForKnight(self, x, y):
+        moves = []
+
+        # Up moves
+        if y + 2 < 8:
+            if x + 1 < 8:
+                if not self.board[x + 1][y + 2]:
+                    moves.append((x + 1, y + 2))
+                elif self.board[x + 1][y + 2][0] != self.turn:
+                    moves.append((x + 1, y + 2)) # Capture
+        if y + 1 < 8:
+            if x + 2 < 8:
+                if not self.board[x + 2][y + 1]:
+                    moves.append((x + 2, y + 1))
+                elif self.board[x + 2][y + 1][0] != self.turn:
+                    moves.append((x + 2, y + 1)) # Capture
+        if y - 1 >= 0:
+            if x + 2 < 8:
+                if not self.board[x + 2][y - 1]:
+                    moves.append((x + 2, y - 1))
+                elif self.board[x + 2][y - 1][0] != self.turn:
+                    moves.append((x + 2, y - 1)) # Capture
+        if y - 2 >= 0:
+            if x + 1 < 8:
+                if not self.board[x + 1][y - 2]:
+                    moves.append((x + 1, y - 2))
+                elif self.board[x + 1][y - 2][0] != self.turn:
+                    moves.append((x + 1, y - 2)) # Capture
+
+        # Down moves
+        if y - 2 >= 0:
+            if x - 1 >= 0:
+                if not self.board[x - 1][y - 2]:
+                    moves.append((x - 1, y - 2))
+                elif self.board[x - 1][y - 2][0] != self.turn:
+                    moves.append((x - 1, y - 2)) # Capture
+        if y - 1 >= 0:
+            if x - 2 >= 0:
+                if not self.board[x - 2][y - 1]:
+                    moves.append((x - 2, y - 1))
+                elif self.board[x - 2][y - 1][0] != self.turn:
+                    moves.append((x - 2, y - 1)) # Capture
+        if y + 1 < 8:
+            if x - 2 >= 0:
+                if not self.board[x - 2][y + 1]:
+                    moves.append((x - 2, y + 1))
+                elif self.board[x - 2][y + 1][0] != self.turn:
+                    moves.append((x - 2, y + 1))
+        if y + 2 < 8:
+            if x - 1 >= 0:
+                if not self.board[x - 1][y + 2]:
+                    moves.append((x - 1, y + 2))
+                elif self.board[x - 1][y + 2][0] != self.turn:
+                    moves.append((x - 1, y + 2))
+
+        return moves
+    
+    # Precondition: bishop at x, y
+    def getValidMovesForBishop(self, x, y):
+        moves = []
+
+        # Diagonal moves to the right up
+        for i in range(1, 8):
+            if x + i < 8 and y + i < 8:
+                if not self.board[x + i][y + i]:
+                    moves.append((x + i, y + i))
+                elif self.board[x + i][y + i][0] != self.turn:
+                    moves.append((x + i, y + i)) # Capture
+                    break
+                else:
+                    break
+
+        # Diagonal moves to the left up
+        for i in range(1, 8):
+            if x - i >= 0 and y + i < 8:
+                if not self.board[x - i][y + i]:
+                    moves.append((x - i, y + i))
+                elif self.board[x - i][y + i][0] != self.turn:
+                    moves.append((x - i, y + i)) # Capture
+                    break
+                else:
+                    break
+
+        # Diagonal moves to the right down
+        for i in range(1, 8):
+            if x + i < 8 and y - i >= 0:
+                if not self.board[x + i][y - i]:
+                    moves.append((x + i, y - i))
+                elif self.board[x + i][y - i][0] != self.turn:
+                    moves.append((x + i, y - i)) # Capture
+                    break
+                else:
+                    break
+
+        # Diagonal moves to the left down
+        for i in range(1, 8):
+            if x - i >= 0 and y - i >= 0:
+                if not self.board[x - i][y - i]:
+                    moves.append((x - i, y - i))
+                elif self.board[x - i][y - i][0] != self.turn:
+                    moves.append((x - i, y - i)) # Capture
+                    break
+                else:
+                    break
+
+        return moves
+        
+    # Precondition: queen at x, y
+    def getValidMovesForQueen(self, x, y):
+        return self.getValidMovesForRook(x, y) + self.getValidMovesForBishop(x, y)
+    
+    # Precondition: king at x, y
+    def getValidMovesForKing(self, x, y):
+        moves = []
+
+        # Up moves
+        if y + 1 < 8:
+            if not self.board[x][y + 1]:
+                moves.append((x, y + 1))
+            elif self.board[x][y + 1][0] != self.turn:
+                moves.append((x, y + 1)) # Capture
+        if y + 1 < 8 and x + 1 < 8:
+            if not self.board[x + 1][y + 1]:
+                moves.append((x + 1, y + 1))
+            elif self.board[x + 1][y + 1][0] != self.turn:
+                moves.append((x + 1, y + 1)) # Capture
+        if y + 1 < 8 and x - 1 >= 0:
+            if not self.board[x - 1][y + 1]:
+                moves.append((x - 1, y + 1))
+            elif self.board[x - 1][y + 1][0] != self.turn:
+                moves.append((x - 1, y + 1)) # Capture
+
+        # Down moves
+        if y - 1 >= 0:
+            if not self.board[x][y - 1]:
+                moves.append((x, y - 1))
+            elif self.board[x][y - 1][0] != self.turn:
+                moves.append((x, y - 1)) # Capture
+        if y - 1 >= 0 and x + 1 < 8:
+            if not self.board[x + 1][y - 1]:
+                moves.append((x + 1, y - 1))
+            elif self.board[x + 1][y - 1][0] != self.turn:
+                moves.append((x + 1, y - 1)) # Capture
+        if y - 1 >= 0 and x - 1 >= 0:
+            if not self.board[x - 1][y - 1]:
+                moves.append((x - 1, y - 1))
+            elif self.board[x - 1][y - 1][0] != self.turn:
+                moves.append((x - 1, y - 1)) # Capture
+
+        # Horizontal moves
+        if x + 1 < 8:
+            if not self.board[x + 1][y]:
+                moves.append((x + 1, y))
+            elif self.board[x + 1][y][0] != self.turn:
+                moves.append((x + 1, y)) # Capture
+        if x - 1 >= 0:
+            if not self.board[x - 1][y]:
+                moves.append((x - 1, y))
+            elif self.board[x - 1][y][0] != self.turn:
+                moves.append((x - 1, y)) # Capture
 
         return moves
     
